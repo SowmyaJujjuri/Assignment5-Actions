db1a847d5ac71bdb911f9b3858ccf6ca0a4a10a7
Sowmya Jujjuri <sowmya.jujjuri@gmail.com>
2020-02-26 19:00:11 -0800

    list function

--- task.py
+++ task.py
@@ -4,3 +4,8 @@ def firstrun():
 
 def area(radius):
     return 3.14*pow(radius, 2)
+
+
+def lastElement(arr):
+    result = [arr[0], arr[len(arr)-1]]
+    return result
--- tests.py
+++ tests.py
@@ -16,6 +16,13 @@ class TestCase(unittest.TestCase):
         radius = 3
         self.assertEqual(28.26, task.area(radius))
 
+    def test_lastElement(self):
+        arr = [1, 2, 3, 4, 5, 6, 7, 8]
+        self.assertEqual([1, 8], task.lastElement(arr))
+
+        arr2 = ["hi", "what", "bye"]
+        self.assertEqual(["hi", "bye"], task.lastElement(arr2))
+        
 
 if __name__ == '__main__':
     unittest.main()
